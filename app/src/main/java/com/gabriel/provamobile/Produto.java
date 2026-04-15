42a56bd7ad1c611d801ad028be707e623d4a5dbf
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-04-15 19:11:19 -0300

    Criando a classe Produto, com suas respectivas entidades

---
+++ app/src/main/java/com/gabriel/provamobile/Produto.java
@@ -0,0 +1,17 @@
+package com.gabriel.provamobile;
+
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+
+import java.util.List;
+@Entity
+public class Produto {
+
+    @PrimaryKey
+
+
+
+}
