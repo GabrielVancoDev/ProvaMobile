b5b8b937aba3b122d20ced8a5e95714037b03b0e
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-04-15 19:53:09 -0300

    Criando variáveis que recebem os dados digitados

--- app/src/main/java/com/gabriel/provamobile/MainActivity.java
+++ app/src/main/java/com/gabriel/provamobile/MainActivity.java
@@ -39,5 +39,36 @@ public class MainActivity extends AppCompatActivity {
         Button btnSalvar = findViewById(R.id.btnSalvar);
         Button btnLista = findViewById(R.id.btnLista);
 
+
+        // Banco de dados
+        ProductDatabase db = Room.databaseBuilder(
+                getApplicationContext(),
+                ProductDatabase.class,
+                "product-db"
+        ).allowMainThreadQueries().build();
+
+        // Banco de dados
+        ProductDatabase db = Room.databaseBuilder(
+                getApplicationContext(),
+                ProductDatabase.class,
+                "product-db"
+        ).allowMainThreadQueries().build();
+
+        productDao = db.productDao();
+
+//        Botão de Salvar Produtos
+        btnSalvar.setOnClickListener(v -> {
+            Log.d("MainActivity", "Botão salvar clicado");
+
+//            Criando variáveis que recebem os dados digitados
+            String nome = etNome.getText().toString().trim();
+            String codigo = etCodigo.getText().toString().trim();
+            String precoStr = etPreco.getText().toString().trim();
+            String quantidadeStr = etQuantidade.getText().toString().trim();
+
+
+
+        });
     }
+
 }
